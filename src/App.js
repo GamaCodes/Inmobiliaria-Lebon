bd8936c3aec65af0116ec8235c157fc21702616d
ASdeveloper-app <arturo.araujo.alvarez@gmail.com>
2021-07-29 19:14:07 -0500

    mini

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from "./config.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="App-link">PROXIMAMENTE</p>
-        <p>Sitio en construcción</p>
+        <img src="/lebon_logo.svg" alt="Lebon" />
+        <p className="App-link">En construcción</p>
+        <img src="/lebonn.svg" alt="Lebon" />
+        <p className="App-link">LEBON | 2021</p>
       </header>
     </div>
   );
